825ed606c633c5520d98ccc042ee4163bf67d5be
Hassan Ehsani <Hassan.ehsani.am@gmail.com>
2025-10-03 16:33:57 +0300

    Initial Commit

---
+++ AppPrototype/AppPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPrototypeApp.swift
+//  AppPrototype
+//
+//  Created by Hassan Ehsani on 03/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppPrototypeTests/AppPrototypeTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppPrototypeTests.swift
+//  AppPrototypeTests
+//
+//  Created by Hassan Ehsani on 03/10/2025.
+//
+
+import Testing
+
+struct AppPrototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
